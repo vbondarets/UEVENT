09098b93a63493c45ad912fd001dd633049e6e7b
Vladimir Bondarets <vbondarets@Vladimirs-MacBook-Pro.local>
2023-04-13 22:56:02 +0300

    configs

--- server/models/db.js
+++ server/models/db.js
@@ -2,8 +2,8 @@ const {Sequelize} = require('sequelize');
 
 module.exports = new Sequelize(
     'uevent', 
-    'vbondarets',
-    'securepass',
+    'root',
+    'root',
     {
         host: '127.0.0.1',
         port: '3306',
