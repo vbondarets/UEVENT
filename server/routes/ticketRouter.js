06878e1def0f97c9810e0c5cd7e2d9fa34e63f2d
Vladimir Bondarets <vbondarets@Vladimirs-MacBook-Pro.local>
2023-04-14 19:26:50 +0300

    some fixes

--- server/routes/ticketRouter.js
+++ server/routes/ticketRouter.js
@@ -2,7 +2,7 @@ const Router = require('express');
 const router = new Router();
 const ticketController = require('../controllers/ticketController');
 
-router.post('/check/:token', ticketController.check);
+router.get('/check/:token', ticketController.check);
 router.get('/list/:event_Id', ticketController.getAllUsers)
 
 module.exports = router;
