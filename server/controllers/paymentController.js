dd99b81b7a9fee31267c5c94d8f79c0295ad45ca
Vladimir Bondarets <vbondarets@Vladimirs-MacBook-Pro.local>
2023-04-14 20:33:34 +0300

    some fixes

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -71,7 +71,7 @@ class PaymentController {
                         }
                     });
                     if (Event[0].tickets_count > 0) {
-                        const seqToken = JwtGenerator(JSON.parse(req.body.merchant_data));
+                        const seqToken = JwtGenerator(merchant_data);
                         const path = await PdfGenerator(seqToken, Event[0], User[0]);
                         await fileMailingService(User[0].email, path);
                         EventModel.update(
